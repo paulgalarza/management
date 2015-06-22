0221f4447a8b5134a8ff14cf1e7e9b0ad6ae16b2
paulgalarza <paulgalarza>
2015-06-21 22:51:55 -0600

    update

--- public/js/controllers/Company.js
+++ public/js/controllers/Company.js
@@ -2,12 +2,18 @@
     'use strict';
     angular
         .module('sidcasoft')
-        .controller('CompaniesController', function($scope, $routeParams, Companies) {
-            $scope.company = Companies.get({
+        .controller('CompanyController', function($scope, $routeParams, Companies) {
+           Companies.get({
                 id: $routeParams.companyId
-            }, function() {
-            	$scope.company._status = $scope.company.status && true;
+            }, function(company) {
+                $scope.company = company;
+            	init();
             });
+
+            var init = function(){
+                $scope.company._status = $scope.company.status && true;
+                $scope.changeStatus();
+            }
 
             $scope.save = function() {
             	$scope.company.status = $scope.company._status ? 1 : 0;
@@ -16,9 +22,18 @@
                         id: company.id
                     }, function(company) {
                     	$scope.company = company;
+                        init();
                     	swal("Guardado exitoso!", "Los cambios han sido guardados correctamente!", "success");
                     });
                 });
+            };
+
+            $scope.getStatus = function(status) {
+                return status ? 'Activo' : 'Cancelado';
+            };
+
+            $scope.changeStatus = function(){
+                $scope.company.strStatus = $scope.getStatus($scope.company._status);
             };
 
         });
