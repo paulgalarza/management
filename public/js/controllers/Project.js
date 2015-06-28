99ce6f1d4701732fd220e6a5719b033c78dd71fc
paulgalarza <paulgalarza>
2015-06-27 22:48:08 -0600

    validaciones

--- public/js/controllers/Project.js
+++ public/js/controllers/Project.js
@@ -2,7 +2,7 @@
     'use strict';
     angular
         .module('sidcasoft')
-        .controller('ProjectController', function($scope, $routeParams, $location, Projects, Customers) {
+        .controller('ProjectController', function($scope, $routeParams, $location, Projects, Customers, Processes) {
             Projects.get({
                 id: $routeParams.projectId
             }, function(project) {
@@ -11,8 +11,13 @@
             });
 
             var init = function() {
+                $scope.selectedTab = 1;
                 $scope.customers = Customers.query();
                 $scope.project._status = $scope.project.status && true;
+                $scope.process = {
+                    requirement_id: 0
+                };
+
             }
 
             $scope.save = function() {
@@ -21,6 +26,33 @@
                     init();
                     swal("Guardado exitoso!", "Los cambios han sido guardados correctamente!", "success");
                 });
+            };
+
+            $scope.selectTab = function(tab){
+                $scope.selectedTab = tab;
+            }
+
+            $scope.new = function(){
+                switch(tab){
+                    case 2:
+                        $('#processModal').openModal();
+                        break;
+                }
+            };
+
+            $scope.newProcess = function(){
+                $('#processModal').openModal();
+            };
+
+            $scope.saveProcess = function(process){
+                $scope.error = 'has-error';
+                if($scope.processForm.$valid){
+                    $('#processModal').closeModal();    
+                    $scope.error = '';
+                    Processes.$save(process, function(process){
+                        //initProcess();
+                    });
+                }
             };
 
             angular.element(document).ready(function() {
